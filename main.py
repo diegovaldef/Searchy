f8cb21b61026c39d1667645e13e90d4b19ed23d4
Diego Valdes <dovalfu@gmail.com>
2024-01-27 17:31:53 -0600

    add ask_products

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from scrapper import Scrapper
+import inflect
 import mysites
 
 amazon = mysites.Amazon()
 ml = mysites.ML()
+p = inflect.engine()
 
+MAX_PRODUCT_QTY = 5
 
 # user_prompts = {
     
@@ -26,14 +29,35 @@ ml = mysites.ML()
 
 
 def ask_products():
-    ...
-
+    
+    products = []
+    welcome()
+    
+    while len(products) < MAX_PRODUCT_QTY:
+        product = input(f"Write your {p.ordinal(len(products))} product")
+        
+        if product == "finish":
+            break
+        
+        products.append(product)
+        
+    return products
+        
+        
 def ask_stores():
     ...
 
 def start_scrapper():
     
     Scrapper()
+    
+    
+def welcome():
+    
+    print("Welcome!, here some reminders:\n")
+    print("- You can only search for a max of 5 products")
+    print("- Write 'finish' to complete the writing of products")
+    print("(if you don't want 5)\n\n")
     
     
 def main():
