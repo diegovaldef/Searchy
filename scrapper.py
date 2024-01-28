2b61514c63dccfdecead6e1c5be996a9aeb34cc5
Diego Valdes <dovalfu@gmail.com>
2024-01-27 21:35:38 -0600

    logs deleted

--- scrapper.py
+++ scrapper.py
@@ -110,6 +110,7 @@ class Scrapper:
             
             options = webdriver.ChromeOptions()
             options.add_argument("--headless")
+            options.add_argument("log-level=3") 
                         
             self.driver = webdriver.Chrome(options=options)            
                         
