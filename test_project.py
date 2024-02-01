f4d2b8d4ac5faee87f25b75848c49f4fc63626e6
Diego Valdes <dovalfu@gmail.com>
2024-01-31 21:06:55 -0600

    first test

--- test_project.py
+++ test_project.py
@@ -1,8 +1,12 @@
 from project import *
 import pytest
 
-def test_ask_products():
-    assert ask_products()
+def test_check_products():
+    assert check_products("finish", ["Laptop", "Laptop HP"]) == True
+    assert check_products("Laptop", ["Mouse"]) == False
+    assert check_products("Laptop", []) == False
+    with pytest.raises(SystemExit):
+        check_products("finish", []) 
 
 
 
